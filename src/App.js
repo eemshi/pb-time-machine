30f6595f714ad4a45bcec46177fc4b992fa21e79
eemshi <oreneemshi@gmail.com>
2019-09-22 22:37:12 -0600

    add bg, logo, nav text

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
 
 function App() {
+  const [isVertical, setIsVertical] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsVertical(window.innerHeight > window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return _ => {
+      window.removeEventListener('resize', handleResize)
+    }
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={isVertical ? "landing vertical" : "landing"}>
+        <div className={"landing-left"}>
+          <img src="/img/tmhas-logo.png" alt="TMHAS logo"/>
+        </div>
+        <div className="landing-right">
+          <h1>PARTICIPATORY BUDGETING</h1>
+          <h2>→ Cole Has a Soul</h2>
+          <h2>→ Auraria Campus</h2>
+          <h1>ART INSTALLATION</h1>
+          <h1>PRESS</h1>
+        </div>
+      </div>
     </div>
   );
 }
